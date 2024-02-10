ee22a56f216fc57d259aa216c251e848cad24a08
Anandvp92 <anandvp92@gmail.com>
2024-02-11 00:12:44 +0530

    add the scroll feature

--- Home_Frontend/src/Home.jsx
+++ Home_Frontend/src/Home.jsx
@@ -4,10 +4,11 @@ import images from './images/priscilla-du-preez-g86airJZ4Gs-unsplash.jpg';
 import { Link, animateScroll as scroll } from "react-scroll";
 import { Footer } from './Footer';
 export function Home(){
- 
+
+
     const [sectionImages, setSectionImages] = useState({
         Home: 'https://media.istockphoto.com/id/1442473224/photo/happy-young-beautiful-woman-having-a-coffee-or-tea-at-home.webp?b=1&s=170667a&w=0&k=20&c=AM0SzdSuN-h9T8GBvGxdeBJsxjhBgBeoOxO_2RSzkMs=',
-        StorySection: 'https://images.pexels.com/photos/4194857/pexels-photo-4194857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        StorySection: 'https://img.freepik.com/premium-photo/abstract-red-black-gradient-plain-studio-background_570543-10303.jpg',
         Flavours: 'https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         Upcomming: 'https://images.pexels.com/photos/3648850/pexels-photo-3648850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         OnDemand: 'https://e1.pxfuel.com/desktop-wallpaper/483/252/desktop-wallpaper-dark-tech-black-technology.jpg',
@@ -18,61 +19,69 @@ export function Home(){
         <>
 
 {/* Start Home page */}
-<section classNameName='Home'>
+<section  id="section1" classNameName='Home'>
 <div className="cover" style={{ backgroundImage: `url(${images})` }}>
     
-<h1>Home</h1>
+<h1 className='titleheading'>Partner Webinar Series</h1>
+<h3 className="titleheading2">Coffe Conversation for Thought Leaders</h3>
+<button>Know More</button>
 
 </div>
 </section>
 {/* End Home page */}
 
 
-<section classNameName='Story-Section'>
+<section  id='section2' classNameName='Flavours'>
 <div className="cover" style={{ backgroundImage: `url(${sectionImages.StorySection})` }}>
-<h1>Story Section</h1>
+<h4 className='heading'>Stirring Up Great Conversations Over Coffe!</h4>
+<p class="para">
+  From a variety of topics to choose from, every talk in the series corresponds to a flavour and an intensity.<br/>
+  Blend the flavor with the intensity you enjoy and get the conversation of your choice.
+</p>
+
+<div className='imagebox'>
+{/* <img className='storyimage' src='https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img> */}
+
+<iframe width="560" height="315"  src="https://www.youtube.com/embed/mMpEt1Y6UVU?si=ncW1vD7_QpVTQy2N" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+
+
+
 </div>
 </section>
 
-<section classNameName='Flavours'>
+<section id='section3' classNameName='Upcoming'>
 <div className="cover" style={{backgroundImage:`url(${sectionImages.Flavours})`}}>
 <h1>Flavours</h1>
 </div>
 </section>
 
-<section classNameName='Upcomming'>
-<div className="cover" style={{backgroundImage:`url(${sectionImages.Upcomming})`}}>
-<h1>Upcomming</h1>
-
-</div>
-</section>
 
 
 
-<section classNameName='On-demand'>
+<section id='section4' classNameName='On-demand'>
 <div className="cover" style={{backgroundImage:`url(${sectionImages.OnDemand})`}}>
 <h1>On demand</h1>
 </div>
 </section>
 
 
-<section classNameName='Faqs'>
+<section id='section5' classNameName='Faqs'>
 <div className="cover" style={{backgroundImage:`url(${sectionImages.Faqs})`}}>
 <h1>FAQS</h1>
 </div>
 </section>
 
-<section classNameName='Contact'>
+<section id='section6' classNameName='Contact'>
 <div className="cover" style={{ backgroundImage: `url(${sectionImages.Contact})` }}>
 <h1>Contact</h1>
 </div>
 </section>
 
 
-<section classNameName='Contact'>
-    <div>
+<section id='section7'>
 <Footer/>
-</div>
 </section>
 
         </>
--- Home_Frontend/src/Navbar.jsx
+++ Home_Frontend/src/Navbar.jsx
@@ -1,48 +1,50 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react";
 import './Navbar.css';
 import logo from './images/logo.svg';
 import { Link } from "react-router-dom";
-export function Navbar(){
-    return(       
-   
- <>
-  <nav className="navbar"> 
-    <div className="navbar-container container">
-      <input type="checkbox" name="" id="" />
-      <div className="hamburger-lines">
-        <span className="line line1" />
-        <span className="line line2" />
-        <span className="line line3" />
-      </div>
-      <ul className="menu-items">
-        <li>
-          <a href="/Home">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Category</a>
-        </li>
-        <li>
-          <a href="#">Menu</a>
-        </li>
-        <li>
-          <a href="#">Testimonial</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-        <li>
-        <a href="/Home" className="logo"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Trend_Micro_logo.svg/2560px-Trend_Micro_logo.svg.png" width={80} height={30} /></a>
-        </li>
-      </ul>
-      <a href="/Home" className="logo"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Trend_Micro_logo.svg/2560px-Trend_Micro_logo.svg.png" width={80} height={30} /></a>
-    </div>
-  </nav>
-</>
-      
-      
-         
-    )
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
+
+export function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container container">
+          <input type="checkbox" name="" id="" />
+          <div className="hamburger-lines">
+            <span className="line line1" />
+            <span className="line line2" />
+            <span className="line line3" />
+          </div>
+          <ScrollLink to="section1" smooth={true} duration={800} className="logo">
+            <img src="https://companieslogo.com/img/orig/4704.T_BIG.D-daec07bf.png?t=1660548193" width={80} height={30} />
+          </ScrollLink>
+          <ul className="menu-items">
+            <li>
+              <ScrollLink to="section1" smooth={true} duration={800}>Home</ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="section2" smooth={true} duration={800}>Flavours</ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="section3" smooth={true} duration={800}>Upcoming</ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="section4" smooth={true} duration={800}>On Demand</ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="section5" smooth={true} duration={800}>FAQs</ScrollLink>
+            </li>
+            <li>
+              <button className="contactbutton"><ScrollLink to="section6" smooth={true} duration={800}>Contact us</ScrollLink></button>
+            </li>
+            <li>
+              <ScrollLink to="section1" smooth={true} duration={800} className="logo">
+                <img src="https://companieslogo.com/img/orig/4704.T_BIG.D-daec07bf.png?t=1660548193" width={80} height={30} />
+              </ScrollLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
